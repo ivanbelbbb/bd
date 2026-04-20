4e3425e4c2fba47748fc1c60caccee84d764c1d4
beljaev ivan <imthe3784@gmail.com>
2026-04-20 16:37:54 +0300

    нейминг

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1> потому что pudge </h1>"
+    return "ЗАЙДИ НА /hello/<твое имя>"
 
+@app.route("/hello/<name>")
+    return f`<h1>Привет, {name}!</h1>`
+    
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=5000) 
